cc98a52cb57079190be0fb0de2e56cfce905d838
krill <lpmaze@gmail.com>
2019-09-06 15:32:02 -0400

    working create user with validations

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -5,17 +5,27 @@ const AuthApiService = {
     return fetch(`${config.API_ENDPOINT}/auth/login`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(credentials),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+      body: JSON.stringify(credentials)
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    )
   },
-  
+  async postUser(user) {
+    const res = await fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    if(!res.ok){
+     return res.json().then(e => Promise.reject(e))
+    }
+
+    return res.json()
+  }
 }
 
 export default AuthApiService
